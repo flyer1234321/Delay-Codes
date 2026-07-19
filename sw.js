cafc2e0136e0809cfefbf41df61c7925783735ac
flyer1234321 <98115254+flyer1234321@users.noreply.github.com>
2026-07-19 12:07:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "delay-codes-v7";
+const CACHE = "delay-codes-v8";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
